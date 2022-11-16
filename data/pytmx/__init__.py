e45c71a59177cf8462abe93229e690764b56b9a3
ChoEun <short0720@gmail.com>
2022-11-17 00:15:21 +0900

    __init__~menugui분석

--- data/pytmx/__init__.py
+++ data/pytmx/__init__.py
@@ -2,7 +2,7 @@ from data.pytmx.tmxloader import load_pygame, load_tmx
 from data.pytmx.utils import buildDistributionRects
 from data.pytmx.pytmx import *
 
-__version__ = '2.16.4'
-__author__ = 'bitcraft'
-__author_email__ = 'leif.theden@gmail.com'
-__description__ = 'Map loader for TMX Files - Python 2.7'
+__version__ = '2.16.4'  #버전
+__author__ = 'bitcraft' # 제작자
+__author_email__ = 'leif.theden@gmail.com' # 제작자 이메일
+__description__ = 'Map loader for TMX Files - Python 2.7'  
